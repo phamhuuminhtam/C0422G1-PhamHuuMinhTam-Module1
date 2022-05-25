1c262a2fab61caec419d1897344ad639e205f512
phamhuuminhtam <minhtamk17qth@gmail.com>
2022-05-25 17:09:27 +0700

    bt

---
+++ ss14-ham/bai-tap/qlsp.js
@@ -0,0 +1,30 @@
+var arr=[]
+function display() {
+    var bang='<table border="0">'
+    bang= bang + '<tr>' + '<td colspan="2" style="font-weight: bolder; text-align: left">'+ 'Display Product' + '</td>' + '<td style=" color: red">' + arr.length + " product" +'</td>'
+    for (let i=0;i<arr.length;i++){
+        bang= bang + '<tr>'
+        bang =bang + '<td>' + arr[i] +"</td>"
+        bang =bang + '<td>'+ "<button type='button' onclick='edit("+ i +")'>Edit</button>" +"</td>"
+        bang =bang + '<td>' + "<button type='button'  onclick='deletei("+ i +")'>Delete</button>" +"</td>"
+
+    }
+    bang= bang + '</tr>' +"</table>"
+    document.getElementById("tableProduct").innerHTML=bang
+}
+onload=display()
+function addProduct() {
+    arr.push(document.getElementById('inputProduct').value)
+    document.getElementById('inputProduct').value=""
+    display()
+}
+function deletei(i) {
+    arr.splice(i,1)
+    display()
+}
+function edit(i) {
+    arr[i]=prompt("nhập thông tin mới của sản phẩm")
+    display()
+}
+
+console.log(arr)
